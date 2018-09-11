64ff8c9b543fa651b2cac795768c108a36dcaf4e
David Morgan <david.alexandermorgan@gmail.com>
2018-09-11 00:34:47 +0000

    Generating random questions and answers to those questions. Returning question and answer to output

--- question.rb
+++ question.rb
@@ -1,1 +1,22 @@
 #passes a question and an answer to the game
+
+class Question
+
+    def generate_number
+        rand(1..20)
+    end
+
+    def generate_question_and_answer
+        val1 = generate_number
+        val2 = generate_number
+
+        question = "What is #{val1} + #{val2}"
+        game_answer = val1+val2
+        return question, game_answer
+    end
+
+end
+
+question1 = Question.new
+
+puts question1.generate_question_and_answer
